6d4efb34a19e9fe0a654aa486714f9d3f497044d
Mohammad Jawadi <jawadi@corp.atacama.de>
2018-10-11 16:59:11 +0200

    11.10.2018

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -1,4 +1,7 @@
+import java.util.List;
 import java.util.Scanner;
+
+import treeSumme.Node;
 
 public class BinaryTree {
 	
@@ -46,47 +49,95 @@ public class BinaryTree {
 			}
 		}
 	}
+	
+	/*
+	public Object findName(String call) {
+		Object result = null;
+		int low = 0;
+		int high = a.length - 1;
+		
+		while((low <= high) && (result == null)) {
+			int mid =(low + high) / 2;
+			int cmp = a[mid].getLastName().compareTo(call);
+			
+			if(cmp == 0) {
+				result = a[mid];
+			}else if (cmp < 0) {
+				low = mid +1;
+			}else {
+				high = mid - 1;
+			}
+		}
+		return result;
+	}
+	*/
+	
+	/*
+	public Node finedName(String call) {
+		Node on = root;
+		boolean leftWings = true;
+		boolean rightWings = true;
+		while(on.name != call) {
+			System.out.println("SHitt");
+			if(on.name != call ) {
+				on = on.left;
+				if(on.left == null) {
+					on = on.right;
+				}
+				if(on == null) {
+					return null;
+				}
+			}
+		}
+		return on;
+	}
+	*/
+	
+	
+	
 
+	List<Node> search(String name, Node selected) {
+
+		if (selected != null) {
+			if(selected.name.equals(name)) {
+				
+			}
+			List<Node> left = search(name,selected.left);
+			List<Node> right = search(name,selected.right);
+			return todo;
+		} else {
+			return todo;
+		}
+	}
 
 
     public Node findNode(int key) {
 
     // Start at the top of the tree
-
      Node focusNode = root;
 
     // While we haven't found the Node
-
     // keep looking
-     if(focusNode.data <= key) {
+     if(focusNode.data < key || focusNode.data > key) {
 
-     while (focusNode.data != key) {
+      while (focusNode.data != key) {
 
         // If we should search to the left
-
         if (key < focusNode.data ) {
-
             // Shift the focus Node to the left child
-
             focusNode = focusNode.left;
-
+            if (focusNode == null)
+                return null;
         } else {
-
             // Shift the focus Node to the right child
-
             focusNode = focusNode.right;
-
         // The node wasn't found
-
         if (focusNode == null)
- 
             return null;
         }
+      }
      }
-     }else {
-    	System.out.println("Data has not been found! ");
-     }
-     return null;
+     return focusNode;
     }
     
     public static void main(String[] args) {
@@ -106,29 +157,28 @@ public class BinaryTree {
             // Find the node with key 75
             
             
-            
-            System.out.println("Please enter the number you want tosearch in the tree: ");
+           /*
             Scanner searchNum = new Scanner(System.in);
             Scanner Word = new Scanner(System.in);
-            String inputWord = Word.nextLine();
+            String inputWord;
             int inputNum = 0;
             int outputNum = 0;
-            boolean run = true;
             
-        	inputNum = searchNum.nextInt();
         	
-        	while(run == true) {
-        		
-        		if(inputNum == 0) {
-        	        System.out.println(theTree.findNode(inputNum));
+        	while(true) {
+                System.out.println("Please enter the number you want to search in the tree: ");
+            	inputNum = searchNum.nextInt();
+        		if(inputNum != 0) {
                     System.out.println("\nNode with the key ");
-            		System.out.println("\n \nPlease search another number or type finish to end the search: ");
-            		outputNum++;
+        	        System.out.println(theTree.findNode(inputNum));
+        	        outputNum++;
         		}else {
-        			System.out.println("Please put a number again: ");
+        			System.out.println("Please put a number again: y/n ");
         			inputNum = 0;
         		}
-        		if(inputWord.equals("Finish") || inputWord.equals("Finish")) {
+        		/*
+        		inputWord = Word.nextLine();
+        		if(inputWord.equals("y") || inputWord.equals("Y")) {
         			System.out.println("\n \n \n Search has been finish, the times of the searchs " + outputNum);
         			outputNum = 0;
         			inputNum = 0;
@@ -136,15 +186,15 @@ public class BinaryTree {
         			searchNum.close();
         			Word.close();
         		}
+        		
         	}
-
+            */    
 
     }
 
     
 }
         
-
 
 class Node {
 	int data;
